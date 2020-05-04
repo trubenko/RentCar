43b25b2ff3c655edbd8207b5d6ec1aa20109c203
Mikhailo <mykhailo.proskurin@ctdev.io>
2020-05-04 12:13:08 +0300

    Add webpack

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// Main js file
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+
+console.log("its work");
+
+import './custom'
+import './init'
+//import './jquery' //todo remove?
+import './materialize'
